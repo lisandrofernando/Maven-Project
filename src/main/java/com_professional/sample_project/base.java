fc509b77b0664b8fdb2d12e7f1ed4924235ccd22
Lisandro Fernando <lisandrusfernandus@gmail.com>
2020-08-01 15:15:14 -0500

    Implementation

---
+++ src/main/java/com_professional/sample_project/base.java
@@ -0,0 +1,47 @@
+package com_professional.sample_project;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+/**
+ * Hello world!
+ *
+ */
+public class base {
+	public static WebDriver driver;
+	public Properties prop;
+    public  WebDriver initializeDriver() throws IOException 
+    {
+        prop = new Properties();
+        
+        FileInputStream file = new FileInputStream(System.getProperty("user.dir")+"\\src\\main\\java\\resources\\data.properties");
+        prop.load(file);
+        
+        String browser_Name = System.getProperty("browser");
+        System.out.println("browser_Name");
+        
+        if(browser_Name.equals("chrome")) {
+        	System.setProperty("webdriver.chrome.driver", "");
+        	driver = new ChromeDriver();
+        	
+        }
+        else if(browser_Name.equals("firefox")) {
+        	System.setProperty("webdriver.gecko.driver", "");
+        	driver = new FirefoxDriver();
+        }
+        else if(browser_Name.equals("internetexplorer")) {
+        	System.setProperty("webdriver.ie.driver", "");
+        	driver = new InternetExplorerDriver();
+        }
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        return driver;
+    }
+}
